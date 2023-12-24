d548384fa98206d26576648892893e79c8d39aed
SaroarShahan <saroar.shahan@gmail.com>
2023-12-25 01:05:02 +0600

    Add error utils

---
+++ src/errors/unauthenticated.js
@@ -0,0 +1,11 @@
+const { StatusCodes } = require('../constants/StatusCodes');
+const CustomAPIError = require('./custom-api');
+
+class UnauthenticatedError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+module.exports = UnauthenticatedError;
