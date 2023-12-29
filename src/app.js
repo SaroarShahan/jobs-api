c84c4e52bfaad5a500602b8e3716fffef64592f2
SaroarShahan <saroar.shahan@gmail.com>
2023-12-29 23:36:48 +0600

    Add security

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ require('express-async-errors');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
 const connectDB = require('./configs/connectDB');
 const errorHandler = require('./middleware/errorHandler');
@@ -15,10 +18,19 @@ const jobRouter = require('./routes/jobRouter');
 const app = express();
 
 // Middlewares
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }),
+);
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(xss());
 
 // Routes
 app.use('/api/v1/auth', authRouter);
